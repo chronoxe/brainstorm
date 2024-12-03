9df1d178427effabea0a916068eea532c50236f7
yazdan samari <yazdan.samari@bil.omu.edu.tr>
2024-12-03 11:49:06 +0300

    Create leet code 7.cpp

---
+++ leet code 7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<int> decompose(int x)
+{
+    vector<int> deco;
+    while (x > 0)
+    {
+        deco.push_back(x % 10);
+        x = x / 10;
+    }
+    return deco;
+}
+int revrse(const vector<int>& dec)
+{
+    int num = 0;
+    for (size_t i = 0; i < dec.size(); ++i)
+    {
+        num += dec[i] * pow(10, dec.size() - 1 - i);
+    }
+    return num;
+}
+
+int main()
+{
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+    vector<int> decomposed = decompose(a);
+    int reversedNumber = revrse(decomposed);
+
+    cout << "Reversed number: " << reversedNumber << endl;
+    return 0;
+}
